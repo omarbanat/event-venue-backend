18cb17f2fc88b184f96872cdaf90d47bcd1fd966
Omar Banat <o.j.banat@gmail.com>
2023-11-08 09:58:05 +0200

    Configurations

--- config/database/queries.js
+++ config/database/queries.js
@@ -1,41 +1,45 @@
 const connection = require('./connection');
 
 const CREATE_USERS_TABLE = `CREATE TABLE IF NOT EXISTS users (
-                            ID INT PRIMARY KEY,
-                            fullName VARCHAR(100),
-                            email VARCHAR(255),
-                            password VARCHAR(255),
-                            role varchar(10)); `;
+                            ID INT AUTO_INCREMENT PRIMARY KEY ,
+                            fullName VARCHAR(100) NOT NULL,
+                            email VARCHAR(255) NOT NULL UNIQUE,
+                            password VARCHAR(255) NOT NULL,
+                            role VARCHAR(10) NOT NULL DEFAULT 'user'
+                            );`;
 
 const CREATE_VENUES_TABLE = `CREATE TABLE IF NOT EXISTS venues (
-                            ID INT PRIMARY KEY,
-                            name VARCHAR(255),
-                            description VARCHAR(255),
-                            capacity INT,
-                            image VARCHAR(255),
-                            address varchar(255)); `;
+                            ID INT AUTO_INCREMENT PRIMARY KEY,
+                            name VARCHAR(255) NOT NULL,
+                            description VARCHAR(255) NOT NULL,
+                            capacity INT NOT NULL,
+                            image VARCHAR(255) NOT NULL,
+                            address varchar(255) NOT NULL
+                            );`;
 
 const CREATE_EVENTS_TABLE = `CREATE TABLE IF NOT EXISTS events (
-                            ID INT PRIMARY KEY,
-                            title VARCHAR(255),
-                            date DATE,
-                            ticketPrice INT,
-                            description VARCHAR(255),
-                            venueID INT,
+                            ID INT AUTO_INCREMENT PRIMARY KEY,
+                            title VARCHAR(255) NOT NULL,
+                            date DATE NOT NULL DEFAULT NOW(),
+                            ticketPrice INT NOT NULL,
+                            description VARCHAR(255) NOT NULL,
+                            venueID INT NOT NULL,
                             FOREIGN KEY (venueID)
                             REFERENCES venues(ID)
-                            ON DELETE CASCADE); `;
+                            ON DELETE CASCADE
+                            );`;
 
 const CREATE_RESERVATIONS_TABLE = `CREATE TABLE IF NOT EXISTS reservations (
-                                    ID INT PRIMARY KEY,
-                                    userID INT,
-                                    venueID INT,
+                                    ID INT AUTO_INCREMENT PRIMARY KEY,
+                                    userID INT NOT NULL,
+                                    venueID INT NOT NULL,
                                     FOREIGN KEY (venueID)
                                     REFERENCES venues(ID)
                                     ON DELETE CASCADE,
                                     FOREIGN KEY (userID)
                                     REFERENCES users(ID)
-                                    ON DELETE CASCADE); `;
+                                    ON DELETE CASCADE
+                                    );`;
 const createTables = async () => {
   try {
     const [usersTableCreation] = await connection.query(CREATE_USERS_TABLE);
