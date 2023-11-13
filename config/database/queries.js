61cb2cee169ee7a80d97313f6c6c55d028f33a52
Omar Banat <o.j.banat@gmail.com>
2023-11-13 12:44:03 +0200

    Modify: event table creation date default value

--- config/database/queries.js
+++ config/database/queries.js
@@ -20,7 +20,7 @@ const CREATE_VENUES_TABLE = `CREATE TABLE IF NOT EXISTS venues (
 const CREATE_EVENTS_TABLE = `CREATE TABLE IF NOT EXISTS events (
                             ID INT AUTO_INCREMENT PRIMARY KEY,
                             title VARCHAR(255) NOT NULL,
-                            date DATE NOT NULL DEFAULT NOW(),
+                            date DATE NOT NULL DEFAULT CURRENT_TIMESTAMP(),
                             ticketPrice INT NOT NULL,
                             description VARCHAR(255) NOT NULL,
                             venueID INT NOT NULL,
